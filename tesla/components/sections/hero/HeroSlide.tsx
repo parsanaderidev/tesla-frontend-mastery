0439be5a68dc4b80acf77af3b58f12ffb0f234d8
parsanaderidev <parsanaderi.dev@gmail.com>
2026-02-21 19:32:36 +0330

    Sorting folder structure and Adding Swiperfiles

---
+++ tesla/components/sections/hero/HeroSlide.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HeroSlide() {
+    return (
+        <div>HeroSlide</div>
+    )
+}
+
+export default HeroSlide
---
+++ tesla/components/sections/hero/HeroSwiper.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HeroSwiper() {
+    return (
+        <div>HeroSwiper</div>
+    )
+}
+
+export default HeroSwiper
