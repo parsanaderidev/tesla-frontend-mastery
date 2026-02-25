a557db791cde68ff205367e0a6c324d91a794340
parsanaderidev <parsanaderi.dev@gmail.com>
2026-02-25 23:51:42 +0330

    Completing the Feature part

--- tesla/components/sections/feature/Feature.tsx
+++ tesla/components/sections/feature/Feature.tsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import Image from 'next/image';
 
 function Feature() {
     return (
-        <div>Feature</div>
+        <section className="flex gap-5">
+            {/* Safe Travel Card */}
+            <div className="w-5xl">
+                <div className="relative">
+                    <Image
+                        src={'/tesla-contents/images/Homepage-FSD-Safety-Report-Split.jpg'}
+                        alt='Safety Report'
+                        width={800}
+                        height={800}
+                        quality={100}
+                        className="object-cover w-full rounded-lg"
+                    />
+
+                    <div className="absolute bottom-15">
+                        {/* Content */}
+                        <div className="grid">
+                            <h2 className="pl-10 text-white text-[43px] font-semibold">Travel Safer, Arrive Refreshed</h2>
+                            <p className="pl-9 text-white text-2xl">Full self-Driving (Supervised)</p>
+                        </div>
+
+                        {/* Button */}
+                        <div className="flex gap-2 pl-9 pt-5">
+                            <button className="w-45 h-full bg-blue-600 text-white rounded-md font-semibold hover:bg-blue-700 transition cursor-pointer">
+                                Demo FSD (Supervised)
+                            </button>
+
+                            <button className="w-55 h-12 bg-white text-black rounded-md font-semibold hover:bg-gray-100 transition cursor-pointer">
+                                Learn More
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Feature */}
+            <div className="flex">
+                <div className="w-120 h-135 relative rounded-lg overflow-hidden">
+                    <Image
+                        src={'/tesla-contents/images/Homepage-Features.jpg'}
+                        alt='Safety Report'
+                        width={800}
+                        height={800}
+                        quality={100}
+                        className="object-cover w-full h-full"
+                    />
+
+                    <div className="absolute bottom-15">
+                        {/* Content */}
+                        <div className="grid w-auto">
+                            <h2 className="pl-10 text-white text-[43px] font-semibold leading-13">Feature That <br /> Come Standard</h2>
+                        </div>
+
+                        {/* Button */}
+                        <div className="flex gap-2 pl-9 pt-5">
+                            <button className="w-45 h-12 bg-blue-600 text-white rounded-md font-semibold hover:bg-blue-700 transition cursor-pointer">
+                                Discover
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
-export default Feature
+export default Feature;
