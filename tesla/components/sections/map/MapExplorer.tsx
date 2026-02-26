77762802eee4ea70e8c42a3285c47e23be206b19
parsanaderidev <parsanaderi.dev@gmail.com>
2026-02-26 13:06:12 +0330

    Adding some style small detail

--- tesla/components/sections/map/MapExplorer.tsx
+++ tesla/components/sections/map/MapExplorer.tsx
@@ -22,7 +22,7 @@ function MapExplorer() {
                 {/* Find Your Charge */}
                 <div className="flex flex-col gap-5">
                     {/* Content */}
-                    <div className="grid">
+                    <div className="grid gap-1">
                         <h2 className="text-4xl font-semibold">Find Your Charge</h2>
                         <p className="text-[20px]">View the network of Tesla Supercharge and Destination Chargers available near you.</p>
                     </div>
@@ -41,7 +41,8 @@ function MapExplorer() {
 
                 {/* Icons */}
                 <div className="flex gap-10">
-                    <div className="flex flex-col leading-tight">
+                    {/* SuperCharge */}
+                    <div className="flex flex-col gap-2 leading-tight">
                         <div className="flex gap-2">
                             <h2 className="text-5xl font-semibold">35,691</h2>
 
@@ -51,7 +52,8 @@ function MapExplorer() {
                         <span className="text-[19px]">Superchargers</span>
                     </div>
 
-                    <div className="flex flex-col leading-tight">
+                    {/* Destination Chargers */}
+                    <div className="flex flex-col gap-2 leading-tight">
                         <div className="flex gap-2">
                             <h2 className="text-5xl font-semibold">493</h2>
 
