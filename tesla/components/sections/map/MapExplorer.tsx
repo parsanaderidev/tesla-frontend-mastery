a3ae8860b458f2ee8f1dd49c83d516ad32095358
parsanaderidev <parsanaderi.dev@gmail.com>
2026-02-26 13:02:34 +0330

    Completing the style and structure of Find Your Charge part

--- tesla/components/sections/map/MapExplorer.tsx
+++ tesla/components/sections/map/MapExplorer.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { GiElectric } from "react-icons/gi";
+import { RiOutletFill } from "react-icons/ri";
 
 function MapExplorer() {
     return (
@@ -16,7 +18,50 @@ function MapExplorer() {
             </div>
 
             {/* Find Charger */}
-            <div className=""></div>
+            <div className="w-full flex gap-40 px-10 py-10">
+                {/* Find Your Charge */}
+                <div className="flex flex-col gap-5">
+                    {/* Content */}
+                    <div className="grid">
+                        <h2 className="text-4xl font-semibold">Find Your Charge</h2>
+                        <p className="text-[20px]">View the network of Tesla Supercharge and Destination Chargers available near you.</p>
+                    </div>
+
+                    {/* Button */}
+                    <div className="flex gap-3">
+                        <button className="w-35 h-10 bg-black text-white rounded-md text-[15px] font-semibold cursor-pointer">
+                            View Network
+                        </button>
+
+                        <button className="w-35 h-10 bg-gray-100 text-black rounded-md text-[15px] font-semibold cursor-pointer">
+                            Learn More
+                        </button>
+                    </div>
+                </div>
+
+                {/* Icons */}
+                <div className="flex gap-10">
+                    <div className="flex flex-col leading-tight">
+                        <div className="flex gap-2">
+                            <h2 className="text-5xl font-semibold">35,691</h2>
+
+                            <GiElectric className="w-12 h-12 p-2 rounded-full bg-red-500 text-white" />
+                        </div>
+
+                        <span className="text-[19px]">Superchargers</span>
+                    </div>
+
+                    <div className="flex flex-col leading-tight">
+                        <div className="flex gap-2">
+                            <h2 className="text-5xl font-semibold">493</h2>
+
+                            <RiOutletFill className="w-12 h-12 p-2 rounded-full bg-gray-400 text-white" />
+                        </div>
+
+                        <span className="text-[19px]">Destination Chargers</span>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
