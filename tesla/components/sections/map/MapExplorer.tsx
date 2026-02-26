73df89fc51dafb9e4c5ce4308cb20e790d77e157
parsanaderidev <parsanaderi.dev@gmail.com>
2026-02-26 12:33:51 +0330

    Completing the Mapbox

--- tesla/components/sections/map/MapExplorer.tsx
+++ tesla/components/sections/map/MapExplorer.tsx
@@ -1,10 +1,21 @@
+import Image from "next/image";
+
 function MapExplorer() {
     return (
         <section className="grid">
-            {/* NetworkExplorer Map */}
-            <div className=""></div>
+            {/* MapExplorer */}
+            <div className="cursor-grab">
+                <Image
+                    src={'/tesla-contents/images/MapExplorer.png'}
+                    alt="Map"
+                    width={900}
+                    height={900}
+                    quality={75}
+                    className="object-cover w-full h-full rounded-lg"
+                />
+            </div>
 
-            {/*  */}
+            {/* Find Charger */}
             <div className=""></div>
         </section>
     );
